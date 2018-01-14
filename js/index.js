3d950fadfa08f88409de4f7e7e939986450299ac
DaniellaTodd <daniella.todd@hotmail.com>
2018-01-14 20:11:05 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,24 +1,27 @@
-getQuote();
-
-$("button").click(function() {
-  getQuote();
-});
-
-function getQuote() {
-  $.ajax({
-    url: "https://api.forismatic.com/api/1.0/?",
-    dataType: "jsonp",
-    data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
-    success: function(response) {
-      $("#get-quote").html(
-        "''" + response.quoteText + "''" +
-          "<br/>"
-      );
-      $("#tweet-quote").attr(
-        "href",
-        "https://twitter.com/intent/tweet?hashtags=RandomQuotes " +
-          response.quoteText
-      );
-    }
+$(document).ready(function() {
+  
+  function quoteMachine() {
+    $.ajax({
+      url: "https://andruxnet-random-famous-quotes.p.mashape.com/",
+      type: "post",
+      headers: {
+        "X-Mashape-Key": "1Xuhcdh7NsmshWXJ9mNNa3ChMDpvp1fIoBNjsnLnPJiwUxrMGO"
+      },
+      dataType: "json",
+      success: function(quoteRecieved) {
+        $("#get-quote").text('~' + quoteRecieved.quote + '~');
+        $("#tweet-quote").attr(
+          "href",
+          "https://twitter.com/intent/tweet?ref_src=twsrc%5Etfw&text= " +
+            quoteRecieved.quote
+        );
+      }
+    });
+    
+  }
+  //when button is clicked, quote should change
+  $("button").click(function() {
+    quoteMachine();
   });
-}
+  quoteMachine();
+});
